b5a1e51a4aa3a51d0b6aa22c38d9a2ef6c5a943c
Rich Robertson <142411947+RichieRichRobertson@users.noreply.github.com>
2024-10-09 15:54:10 -0400

    Add files via upload

--- lua/lush_theme/TentCraft-Dark.lua
+++ lua/lush_theme/TentCraft-Dark.lua
@@ -139,7 +139,7 @@ local hsl = lush.hsl
      Question       { fg=success}, -- |hit-enter| prompt and yes/no questions
      QuickFixLine   { bg=LTdarkblue.lighten(10), fg=success}, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
      Search         { fg = LTWhite, bg = link.darken(10) }, -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
-    SpecialKey     { fg=Sentry, bg=logosecondary }, -- Unprintable characters: text displayed differently from what it really is. But not 'listchars' whitespace. |hl-Whitespace|
+    SpecialKey     { fg=Sentry, bg=logoSecondary }, -- Unprintable characters: text displayed differently from what it really is. But not 'listchars' whitespace. |hl-Whitespace|
     SpellBad       {fg=danger, gui="underline" }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
     SpellCap       { fg=sparks, gui="underline"}, -- Word that should start with a capital. |spell| Combined with the highlighting used otherwise.
     SpellLocal     { fg=success, gui="underline"}, -- Word that is recognized by the spellchecker as one that is used in another region. |spell| Combined with the highlighting used otherwise.
